9492b00ff2276f488fb5a7a9c31c75065cb1fb96
Paul N. Baker <paul.nelson.baker@gmail.com>
2018-08-19 06:37:23 -0600

    Better testing. Not just printing to console, making true assertions.

--- BallDataStructures_test.go
+++ BallDataStructures_test.go
@@ -3,7 +3,6 @@ package main
 import (
 	"testing"
 	"github.com/paul-nelson-baker/ball-clock-simulator/structure"
-	"fmt"
 )
 
 func TestClockMatchesPDFIterations(t *testing.T) {
@@ -11,7 +10,10 @@ func TestClockMatchesPDFIterations(t *testing.T) {
 	for i := 0; i < 325; i++ {
 		ballClock.TickMinute()
 	}
-	fmt.Println(ballClock.String())
+	testSlicesEqual(t, ballClock.Min, []int{})
+	testSlicesEqual(t, ballClock.FiveMin, []int{22, 13, 25, 3, 7})
+	testSlicesEqual(t, ballClock.Hour, []int{6, 12, 17, 4, 15})
+	testSlicesEqual(t, ballClock.Main, []int{11, 5, 26, 18, 2, 30, 19, 8, 24, 10, 29, 20, 16, 21, 28, 1, 23, 14, 27, 9})
 }
 
 func TestClockCycleDaysOne(t *testing.T) {
@@ -24,8 +26,9 @@ func TestClockCycleDaysOne(t *testing.T) {
 			break
 		}
 	}
-	fmt.Printf("Days before reset: %d\n", days)
-	fmt.Println(ballClock.String())
+	if days != 15 {
+		t.FailNow()
+	}
 }
 
 func TestClockCycleDaysTwo(t *testing.T) {
@@ -38,6 +41,21 @@ func TestClockCycleDaysTwo(t *testing.T) {
 			break
 		}
 	}
-	fmt.Printf("Days before reset: %d\n", days)
-	fmt.Println(ballClock.String())
+	if days != 378 {
+		t.FailNow()
+	}
+}
+
+func testSlicesEqual(t *testing.T, a, b []int) {
+	if (a == nil) != (b == nil) {
+		t.FailNow()
+	}
+	if len(a) != len(b) {
+		t.FailNow()
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			t.FailNow()
+		}
+	}
 }
